37bef67b1e428f6f1d6d0beba04233ff074c2dee
Christian <christirom10@gmail.com>
2022-09-02 12:47:42 -0300

    Se agregaron datos al objeto Producto

--- producto.js
+++ producto.js
@@ -3,27 +3,25 @@ function change_image(image) {
   container.src = image.src;
 }
 
-let recolectar_datos = function() {
+let recolectar_datos = function () {
   let producto = {}
   producto.precio_descuento = document.querySelector('.act-price').innerText
   producto.precio_lista = document.querySelector('.dis-price').innerText
   producto.nombre = document.querySelector('#nombre').innerText
   producto.cantidad = 0
-  producto.talle = 
+  producto.talle = document.querySelector('input[type=radio]:checked')
   producto.marca = document.querySelector('.brand').innerText
   producto.fotos = [
     'https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwb9ac9d2d/products/DG_QCACPU001IB/DG_QCACPU001IB-3.JPG',
     'https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwfd0492d7/products/DG_QCACPU001IB/DG_QCACPU001IB-1.JPG',
     'https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw77a26a9a/products/DG_QCACPU001IB/DG_QCACPU001IB-4.JPG'
   ]
+}
 
-  let formas_pago = {}
+console.log(producto)
+
+let formas_pago = {}
   formas_pago.tarjeta = "Visa"
   formas_pago.efectivo = "Rapipago"
-
-  
-  console.log(producto)
-  
-}
 
 recolectar_datos
