12f5ca9c067a48d41f4cafaacf89ebaef3cfa413
Christian <christirom10@gmail.com>
2022-09-11 18:33:09 -0300

    Se agrego funcion de agregar varios productos

--- carrito.js
+++ carrito.js
@@ -10,6 +10,10 @@ let recuperarData = function() {
     
     console.log('Nombre del producto: ')
     console.log(producto.nombre)
+
+    document.querySelector('.item-name').innerText = producto.nombre
+    document.querySelector('.item-desc').innerText = producto.descripcion
+    document.querySelector('.item-price').innerText = producto.precio_lista                                 
 }
 
 recuperarData()
--- producto.js
+++ producto.js
@@ -4,11 +4,12 @@ function change_image(image) {
 }
 
 let recolectar_datos = function () {
-  let producto = {}
+  let producto = {};
   producto.precio_descuento = document.querySelector('.act-price').innerText
   producto.precio_lista = document.querySelector('.dis-price').innerText
   producto.nombre = document.querySelector('#nombre').innerText
   producto.cantidad = 0
+  producto.descripcion = document.querySelector('.about').innerText
   producto.descripcion = document.querySelector('.about').innerText
   producto.talle = document.querySelector('input[type=radio]:checked').value
   producto.fotos = ['https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw7a39f4bf/products/DG_DUOJEU010B/DG_DUOJEU010B-3.JPG',
@@ -16,16 +17,44 @@ let recolectar_datos = function () {
       'https://www.dexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwf6158278/products/DG_DUOJEU010B/DG_DUOJEU010B-1.JPG'
   ]
 
-  console.log(producto)
-  localStorage.setItem('miProducto', JSON.stringify(producto) )
+  console.log(producto);
 
-}
+  let listado = [];
 
-recolectar_datos()
+  if (localStorage.getItem("miProducto")) {
+    listado = JSON.parse(localStorage.getItem("miProducto"));
+    console.log(
+      "Tomamos lo que este en el local storage y lo ponemos en el array: "
+    );
+    console.log(listado);
+  }
 
-let probamosLocalStorage = function() {
-  localStorage.setItem('precio',document.querySelector('.act-price').innerText)
-  localStorage.setItem('nombre',document.querySelector('#nombre').innerText)
-  localStorage.setItem('descuento',document.querySelector('.dis-price').innerText)
-  
-}
+  if (localStorage.getItem("miProducto")) {
+    // Esta definido en el localStorage. Ya tiene productos
+    console.log("Agregamos un producto");
+    listado.push(producto);
+    localStorage.setItem("miProducto", JSON.stringify(listado));
+  } else {
+    // No esta definido en el localStorage. No tiene productos
+    console.log("Creamos el item en el local Storage y agregamos producto");
+    listado.push(producto);
+    localStorage.setItem("miProducto", JSON.stringify(listado));
+  }
+
+  console.log("----------------------");
+  console.log(listado);
+};
+
+recolectar_datos();
+
+let probamosLocalStorage = function () {
+  localStorage.setItem(
+    "precio",
+    document.querySelector(".act-price").innerText
+  );
+  localStorage.setItem("nombre", document.querySelector("#nombre").innerText);
+  localStorage.setItem(
+    "descuento",
+    document.querySelector(".dis-price").innerText
+  );
+};
